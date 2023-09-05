c36164c5262ae053b23fead0a7d09a3752195b06
sharo-jef <sharo.jef@gmail.com>
2023-09-06 03:32:00 +0900

    fix flex

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,7 @@
 <div class="mx-auto w-full container pt-10 px-4 sm:px-6 lg:px-8">
 	<div class="mx-auto grid grid-cols-1 items-start sm:max-w-lg lg:max-w-6xl">
 		<div class="relative rounded-lg text-center mb-6 border dark:border-none dark:bg-gray-800">
-			<form class="p-6 flex gap-4 justify-center">
+			<form class="p-6 flex flex-wrap gap-4 justify-center">
 				<DatePicker bind:date bind:year bind:month bind:day />
 				<select
 					class="h-10 p-2 outline-none border-b border-gray-600 dark:bg-gray-800 dark:text-gray-400"
